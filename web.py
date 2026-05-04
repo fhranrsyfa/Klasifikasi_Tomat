e8366e6923cc15cc8221a49319b477e4be49c064
fhranrsyfa <fhranrsyfa20@gmail.com>
2026-05-04 12:04:11 +0700

    web.py

--- web.py
+++ web.py
@@ -29,11 +29,11 @@ def setup_assets():
         unsafe_allow_html=True
     )
     
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    css_path = os.path.join(current_dir, "style.css")
-    if os.path.exists(css_path):
-        with open(css_path) as f:
-            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    #current_dir = os.path.dirname(os.path.abspath(__file__))
+    #css_path = os.path.join(current_dir, "style.css")
+    #if os.path.exists(css_path):
+        #with open(css_path) as f:
+            #st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 setup_assets()
 
